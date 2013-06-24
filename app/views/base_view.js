08ab99e5eea1b769c054b1aebcc27be1f022bf33
Jack Dubie <jack@orchestra.com>
2013-06-24 15:16:58 -0700

    Moved scrolling into postRender

--- app/views/base_view.js
+++ app/views/base_view.js
@@ -2,4 +2,8 @@ var RendrView = require('rendr/shared/base/view');
 
 // Create a base view, for adding common extensions to our
 // application's views.
-module.exports = RendrView.extend({});
+module.exports = RendrView.extend({
+  postRender: function () {
+    window.scrollTo(0, 0);
+  }
+});
